270f8bee3666d6a4533398aab0a19b559d0fc5ff
Marcel Cruz <marcelcruz@live.com>
2018-09-08 21:43:49 +0100

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io()
 
 socket.on('connect', () => {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    from: 'Marcel',
-    text: 'Hey, I am here!',
-  })
 })
 
 socket.on('disconnect', () => {
