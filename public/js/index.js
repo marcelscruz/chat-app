5b51814c7ed99f1d598b3a96a6135bb4c22ad950
Marcel Cruz <marcelcruz@live.com>
2018-09-10 16:58:30 +0100

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,26 @@
 const socket = io()
 
+const scrollToBottom = () => {
+  // Selectors
+  const messages = jQuery('#messages')
+  const newMessage = messages.children('li:last-child')
+
+  // Heights
+  const clientHeight = messages.prop('clientHeight')
+  const scrollTop = messages.prop('scrollTop')
+  const scrollHeight = messages.prop('scrollHeight')
+  const newMessageHeight = newMessage.innerHeight()
+  const lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight)
+    console.log('scroll')
+  }
+}
+
 socket.on('connect', () => {
   console.log('Connected to server')
 })
@@ -18,6 +39,7 @@ socket.on('newMessage', message => {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 socket.on('newLocationMessage', message => {
@@ -30,6 +52,7 @@ socket.on('newLocationMessage', message => {
   })
 
   jQuery('#messages').append(html)
+  scrollToBottom()
 })
 
 jQuery('#message-form').on('submit', e => {
